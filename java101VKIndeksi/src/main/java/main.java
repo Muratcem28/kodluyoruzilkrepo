6295cbf06a3c2f3ef7d615a057ee369a0552eecd
Murat Cem <muratcemozdemir28@gmail.com>
2022-06-10 01:51:49 +0300

    vucutKitleEndeksi

---
+++ java101VKIndeksi/src/main/java/main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        float boy, kilo, indeks;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lutfen boyunuzu metre cinsinden giriniz : ");
+        boy = input.nextFloat();
+
+        Scanner input1 = new Scanner(System.in);
+        System.out.print("Lutfen kilonuzu giriniz : ");
+        kilo = input1.nextFloat();
+
+        indeks = (kilo/boy/boy);
+        System.out.print("Vucut kitle indeksiniz : " + indeks);
+    }
+}
